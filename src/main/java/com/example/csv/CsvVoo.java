975311eb0accb629335bdfb0a1c24cf0b4f44b2e
jcarpim <jecarpim@gmail.com>
2020-02-11 21:31:24 -0300

    Construtor com fields

--- src/main/java/com/example/csv/CsvVoo.java
+++ src/main/java/com/example/csv/CsvVoo.java
@@ -4,9 +4,15 @@ public class CsvVoo {
     private String origem;
     private String destino;
     private String saida;
-    private String chegada;
+	private String chegada;
 
-    // Gets e Sets omitidos para ficar menor
+	public CsvVoo(String origem, String destino, String saida, String chegada) {
+		super();
+		this.origem = origem;
+		this.destino = destino;
+		this.saida = saida;
+		this.chegada = chegada;
+	}
 
     public String toString() {
         return "CsvVoo{Origem='" + origem + "\', Destino=" + destino + "\', Saida='" + saida + "\' +, Chegada='" + chegada + "\'}";
